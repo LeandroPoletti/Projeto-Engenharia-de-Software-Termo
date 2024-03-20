ba0c4ac4af26e155f69dc1e5374778c98184af9f
Leandro Poletti de Oliveira <leandropoletti2004@gmail.com>
2024-03-20 19:20:55 -0300

    Teste sobre retorno de estado da entrada

--- logica/palavras.py
+++ logica/palavras.py
@@ -14,5 +14,25 @@ class Palavra():
                 self.listaResposta.append(i)
     
     def checkAnswer(self, entrada: str):
+        returnList = []
+        for i in range(len(entrada)):
+            returnList.append(self.__check(entrada[i], i))
             
+        resultado = set(returnList)
+        
+        if len(resultado) == 1 and resultado == tipoResposta.existePosCorreta:
+            self.solved = True
+        
+        return returnList
     
+    def __check(self, letra: str, index: int):
+        if letra == self.listaResposta[index]:
+            return tipoResposta.existePosCorreta
+        elif letra in self.listaResposta:
+            return tipoResposta.existePosErrada
+        else:
+            return tipoResposta.naoExiste
+    
+    def getSolved(self) -> bool:
+        return self.solved
+        
--- logica/tests.py
+++ logica/tests.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from palavras import Palavra
+from tiposResposta import tipoResposta
 
 class PalavraTest(TestCase):
     
@@ -7,5 +8,27 @@ class PalavraTest(TestCase):
         p = Palavra('teste')
         self.assertIsInstance(p, Palavra)
     
-    def test_return_exist(self):
+    def test_return_state_list(self):
+        p = Palavra('Teste')
+        # p.checkAnswer('Teste')
+        expectedReturn = [
+            tipoResposta.existePosCorreta,
+            tipoResposta.existePosCorreta,
+            tipoResposta.existePosCorreta,
+            tipoResposta.existePosCorreta,
+            tipoResposta.existePosCorreta,
+        ]
+        self.assertEqual(p.checkAnswer('Teste'), expectedReturn)
         
+    def test_notice_all_incorrect_value(self):
+        p = Palavra('Teste')
+        entrada = 'lavar'
+        expectedReturn = [
+            tipoResposta.naoExiste,
+            tipoResposta.naoExiste,
+            tipoResposta.naoExiste,
+            tipoResposta.naoExiste,
+            tipoResposta.naoExiste,
+        ]
+        
+        self.assertEqual(p.checkAnswer(entrada), expectedReturn)
